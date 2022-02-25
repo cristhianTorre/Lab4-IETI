3eeca137ead60ac36b91b45c99dcf0480ace6e69
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2022-02-24 22:28:37 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <div>
+            <TextField id="filled-basic" label="Email" variant="filled" />
+          </div>
+          <div>
+            <TextField id="filled-basic" label="Password" variant="filled" />
+          </div>
+          <div>
+            <Button variant="outlined">Login</Button>
+          </div>
+        </header>
     </div>
   );
 }
-
 export default App;
